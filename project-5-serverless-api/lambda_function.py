f4e655e31719957d0cd7a6d8492155a537cd5672
Aung Kyaw Soe <64706759+Agkyaws@users.noreply.github.com>
2026-02-22 22:17:59 +0700

    Implement user data insertion in DynamoDB
    Added functionality to insert user data into DynamoDB.

--- project-5-serverless-api/lambda_function.py
+++ project-5-serverless-api/lambda_function.py
@@ -1,1 +1,25 @@
+import json
+import boto3
+import uuid
 
+# Connect to DynamoDB
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('UserData')
+
+def lambda_handler(event, context):
+    # Create a random ID
+    user_id = str(uuid.uuid4())
+    
+    # Insert data into database
+    table.put_item(
+        Item={
+            'userid': user_id,
+            'message': 'Hello from Serverless World!',
+            'status': 'Success'
+        }
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Successfully saved User ID: {user_id}')
+    }
